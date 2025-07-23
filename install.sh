74949b4a7cb3d009d7fcb66ae9f3922dd41c3444
Sawyer Summerville <143439752+Sawyerville@users.noreply.github.com>
2025-07-23 07:33:28 -0400

    Update install.sh replace "Firefox" and "jailfox" with "Librewolf" and "jailwolf"

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ TARGET=${container:?"Usage: install.sh TARGET [Path]"}
 dir_download=$2
 
 if [ ! -z $2 ]; then
-    bastille cmd $TARGET mkdir -p /home/firefox/Downloads
-    bastille cmd $TARGET chown firefox:firefox /home/firefox/Downloads
-    bastille mount $TARGET $dir_download usr/home/firefox/Downloads nullfs rw 0 0
+    bastille cmd $TARGET mkdir -p /home/librewolf/Downloads
+    bastille cmd $TARGET chown librewolf:librewolf /home/librewolf/Downloads
+    bastille mount $TARGET $dir_download usr/home/librewolf/Downloads nullfs rw 0 0
 fi
 
-sed "s/TARGET/$TARGET/g" jail-firefox-exec > jail-firefox-exec.$TARGET
+sed "s/TARGET/$TARGET/g" jail-librewolf-exec > jail-librewolf-exec.$TARGET
 
-install -m 755 jail-firefox-exec.$TARGET /usr/local/libexec/jail-firefox-exec
-install -m 755 jailfox /usr/local/bin/
-install -m 644 jailfox.desktop /usr/local/share/applications
+install -m 755 jail-librewolf-exec.$TARGET /usr/local/libexec/jail-librewolf-exec
+install -m 755 jailwolf /usr/local/bin/
+install -m 644 jailwolf.desktop /usr/local/share/applications
 update-desktop-database
 
